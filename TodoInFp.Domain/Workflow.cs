b3e63c48491514920f3d1a5f674c5682af01dd85
Johan Burell <johan.burell@gmail.com>
2024-10-22 03:19:11 +0200

    WIP

--- TodoInFp.Domain/Workflow.cs
+++ TodoInFp.Domain/Workflow.cs
@@ -1,13 +1,69 @@
-﻿using CSharpFunctionalExtensions;
+﻿using System.Net;
+using System.Text;
+using System.Text.Json;
+using CSharpFunctionalExtensions;
+using CSharpFunctionalExtensions.ValueTasks;
 using OneOf;
 using TodoInFp.Domain.DomainObjs;
 
 namespace TodoInFp.Domain;
 using CreateTodoError = OneOf<DuplicateItemError, UnknownError>;
+using WorkflowError = OneOf<
+  ServiceNotFound, 
+  CouldNotReadHttpBody,
+  DeserializeError,
+  ItemAlreadyExistsError,
+  UnknownWorkflowError
+>;
+
+public record CouldNotReadHttpBody;
+public record ServiceNotFound;
+public record DeserializeError;
+public record ItemAlreadyExistsError;
+public record UnknownWorkflowError;
 
 public class GetTodoItemsWorkflow(ITodoItemStore store)
 {
   public IQueryable<TodoItem> GetTodos() => store.GetTodoItems();
 
-  public Result<TodoItem, CreateTodoError> CreateTodo(TodoItem item) => store.CreateTodoItem(item);
+  public Result<TodoItem, CreateTodoError> CreateTodoItem(JsonSerializerOptions jsonOpts) => 
+  Result.Success(jsonOpts)
+    .Bind(async jsonOpts =>
+  (await GetJson(jsonOpts, httpCtx))
+  .Map(json => (jsonOpts, json)))
+  .Bind<(JsonSerializerOptions, string), TodoItem, WorkflowError>(
+  val => DeserializeJson(val.Item1, val.Item2))
+    .Bind<TodoItem, TodoItem, WorkflowError>(item =>
+  workflow.CreateTodo(item)
+  .MapError<TodoItem, CreateTodoError, WorkflowError>(err => err.Match<WorkflowError>(
+  duplicate => new ItemAlreadyExistsError(),
+  unknown => new UnknownWorkflowError()))
+    ).Match(
+    onSuccess: item => (int)HttpStatusCode.Created,
+    onFailure: e => e.Value switch
+    {
+      ItemAlreadyExistsError => (int)HttpStatusCode.Conflict,
+      _ => (int)HttpStatusCode.InternalServerError,
+    } 
+  ))
+  
+  private async Task<string> GetRequestBodyAsync(HttpRequest request)
+  {
+    request.EnableBuffering();
+    using var reader = new StreamReader(request.Body, Encoding.UTF8, detectEncodingFromByteOrderMarks: false, leaveOpen: true);
+    var body = await reader.ReadToEndAsync();
+    request.Body.Position = 0;
+    return body;
+  }
+  
+  private Result<TodoItem, CreateTodoError> StoreTodo(TodoItem item) => store.CreateTodoItem(item);
+
+  private Task<Result<string, WorkflowError>> GetJson(JsonSerializerOptions opts, HttpContext httpCtx) =>
+    Result.Try<string, WorkflowError>(async () => await GetRequestBodyAsync(httpCtx.Request), 
+      ex => new CouldNotReadHttpBody());
+
+  private Result<TodoItem, WorkflowError> DeserializeJson(JsonSerializerOptions jsonOpts, string json) =>
+    Result.Try<TodoItem, WorkflowError>(
+      () => JsonSerializer.Deserialize<TodoItem>(json, jsonOpts) ?? throw new InvalidOperationException(), 
+      ex => new DeserializeError());
 }
--- TodoInFp/Api/TodoApi.cs
+++ TodoInFp/Api/TodoApi.cs
@@ -8,68 +8,26 @@ using TodoItem = TodoInFp.Domain.DomainObjs.TodoItem;
 using CreateTodoError = OneOf.OneOf<TodoInFp.Domain.DuplicateItemError, TodoInFp.Domain.UnknownError>;
 using IResult = Microsoft.AspNetCore.Http.IResult;
 using WorkflowError = OneOf.OneOf<
-  TodoInFp.Api.ServiceNotFound, 
-  TodoInFp.Api.CouldNotReadHttpBody,
-  TodoInFp.Api.DeserializeError,
-  TodoInFp.Api.ItemAlreadyExistsError,
-  TodoInFp.Api.UnknownWorkflowError
+  TodoInFp.Domain.ServiceNotFound, 
+  TodoInFp.Domain.CouldNotReadHttpBody,
+  TodoInFp.Domain.DeserializeError,
+  TodoInFp.Domain.ItemAlreadyExistsError,
+  TodoInFp.Domain.UnknownWorkflowError
 >;
 
 namespace TodoInFp.Api;
-
-record CouldNotReadHttpBody;
-record ServiceNotFound;
-record DeserializeError;
-record ItemAlreadyExistsError;
-record UnknownWorkflowError;
 
 public class TodoApi(GetTodoItemsWorkflow workflow)
 {
   public void Setup(RouteGroupBuilder builder)
   {
       builder.MapGet("/", workflow.GetTodos);
-      builder.MapPost("/", async httpCtx => 
-        await GetJsonOptions(httpCtx)
-          .Bind(async jsonOpts =>
-            (await GetJson(jsonOpts, httpCtx))
-            .Map(json => (jsonOpts, json)))
-          .Bind<(JsonSerializerOptions, string), TodoItem, WorkflowError>(
-            val => DeserializeJson(val.Item1, val.Item2))
-          .Bind<TodoItem, TodoItem, WorkflowError>(item =>
-            workflow.CreateTodo(item)
-              .MapError<TodoItem, CreateTodoError, WorkflowError>(err => err.Match<WorkflowError>(
-                duplicate => new ItemAlreadyExistsError(),
-                unknown => new UnknownWorkflowError()))
-          ).Match(
-            onSuccess: item => httpCtx.Response.StatusCode = (int)HttpStatusCode.Created,
-            onFailure: e => e.Value switch
-            {
-              ItemAlreadyExistsError => httpCtx.Response.StatusCode = (int)HttpStatusCode.Conflict,
-              _ => httpCtx.Response.StatusCode = (int)HttpStatusCode.InternalServerError,
-            } 
-          ));
+      builder.MapPost("/", httpCtx => 
+        Task.FromResult(GetJsonOptions(httpCtx).Map(workflow.CreateTodoItem)));
       
-
-    Result<JsonSerializerOptions, WorkflowError> GetJsonOptions(HttpContext httpCtx) =>
-      Result.Try<JsonSerializerOptions, WorkflowError>(() => 
-        httpCtx.RequestServices.GetRequiredService<JsonSerializerOptions>(), 
-        ex => new ServiceNotFound());
-
-    Task<Result<string, WorkflowError>> GetJson(JsonSerializerOptions opts, HttpContext httpCtx) =>
-      Result.Try<string, WorkflowError>(async () => await GetRequestBodyAsync(httpCtx.Request), ex => new CouldNotReadHttpBody());
-
-    Result<TodoItem, WorkflowError> DeserializeJson(JsonSerializerOptions jsonOpts, string json) =>
-      Result.Try<TodoItem, WorkflowError>(
-        () => JsonSerializer.Deserialize<TodoItem>(json, jsonOpts) ?? throw new InvalidOperationException(), 
-        ex => new DeserializeError());
-  }
-
-  private async Task<string> GetRequestBodyAsync(HttpRequest request)
-  {
-    request.EnableBuffering();
-    using var reader = new StreamReader(request.Body, Encoding.UTF8, detectEncodingFromByteOrderMarks: false, leaveOpen: true);
-    var body = await reader.ReadToEndAsync();
-    request.Body.Position = 0;
-    return body;
+      Result<JsonSerializerOptions, WorkflowError> GetJsonOptions(HttpContext httpCtx) =>
+        Result.Try<JsonSerializerOptions, WorkflowError>(() => 
+            httpCtx.RequestServices.GetRequiredService<JsonSerializerOptions>(), 
+          ex => new ServiceNotFound());
   }
 }
